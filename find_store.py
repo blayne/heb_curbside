693ee49888371d24f4f8b62897b6e91101886e42
calebknoll <5396680+calebknoll@users.noreply.github.com>
2020-04-16 00:57:47 -0500

    Do nothing for speak option on non-mac

--- find_store.py
+++ find_store.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sys import exit
+from sys import exit, platform
 from time import sleep
 from dateutil.parser import parse
 from datetime import datetime
@@ -235,7 +235,7 @@ class Search:
         print("email_username: " + str(self.email_username))
 
     def speak_num_curbside_slots(self):
-        if self.speak:
+        if self.speak and platform == 'darwin':
             import mac_say
             if self.num_curbside_slots > 0:
                 if self.detail or self.store_id:
@@ -452,7 +452,7 @@ if __name__ == '__main__':
         default=5)
     parser.add_argument(
         "--speak",
-        help="Speak when a slot is found",
+        help="Speak when a slot is found.  Ignored if not macOS.",
         action='store_true')
     parser.add_argument(
         "--email-to",
